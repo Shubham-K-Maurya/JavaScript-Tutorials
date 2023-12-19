eb76da60c838d9d44a0800e98d703e9b37545a41
Shubham Maurya <107738619+Shubham-K-Maurya@users.noreply.github.com>
2023-12-19 08:12:42 +0000

    variables coomit done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,28 @@
+const accountId=98745
+let accountEmail="skm@gmail.com"
+var accountpassword="123456"
+accountCity="Prayagraj"   //not preferrable but can use it
+
+let accountstate
+
+
+
+// accountId=25   //not allowed
+
+accountEmail="shubham@gmail.com"
+
+accountpassword="985632"
+
+accountCity="varanasi"
+
+
+/*
+    prefer not to use var
+    because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountpassword, accountCity, accountstate])
+
+console.log(accountstate);
