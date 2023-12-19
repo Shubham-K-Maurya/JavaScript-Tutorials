5b77fcf6276d459c1e6c2f612cf136843229baf3
Shubham Maurya <107738619+Shubham-K-Maurya@users.noreply.github.com>
2023-12-19 08:35:32 +0000

    Data types coomit done

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,36 @@
+"use strict";  //treat all JS code as a newer version
+/*
+alert("Hello SKM")   // this can not be done here now either use with html or in browsers console to send a alert pop-up
+alert(3+3)
+ */
+
+// console.log(3+3)  console.log("Shubham")    //this is not in readable format i.e, code reusability should be high
+
+console.log(3+3);  console.log("Shubham");
+
+//2 documentation  1) tc39   2) mdn
+
+let name="Shubham"
+let age=19
+let isLoggedIn=true
+
+//data types 
+
+// number
+// big int
+// string => use in double quotes
+// boolean =>  either true or false
+// null => (standalone value)
+// undefined => representation of empty type
+// symbol => (mostly used in react for finding "uniqueness")
+
+
+// object
+
+
+console.log(typeof "Shubham");   // here typeof is used to find the data type of  variables
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof Object);
+
